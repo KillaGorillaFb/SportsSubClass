6d05b0c23a3da2ec9b6f91f748df693c5b3f4308
Frankie Barrios <softwaredevfb@gmail.com>
2018-09-17 20:24:54 -0500

    .cs only for SportsSubClass

---
+++ Football.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamsLib;
+
+namespace SportSubClass
+{
+    class Football : Team
+    {
+        #region Private Class Fields
+        private string reqTurfType;
+        private string teamLocation;
+        private int reqPlayerWeight;
+        #endregion Private Class Fields
+        
+        #region Public Properties
+        public string ReqTurfType { get => reqTurfType; set => reqTurfType = value; }
+        public string TeamLocation { get => teamLocation; set => teamLocation = value; }
+        public int ReqPlayerWeight { get => reqPlayerWeight; set => reqPlayerWeight = value; }
+        #endregion Public Properties
+
+        #region Constructors
+        /// <summary>
+        /// Default No-Arg
+        /// </summary>
+        public Football()
+            : base ()
+        {
+        }
+
+        /// <summary>
+        /// Creates Complete Football Team
+        /// </summary>
+        /// <param name="tName"></param>
+        /// <param name="sName"></param>
+        /// <param name="cName"></param>
+        /// <param name="rTurf"></param>
+        /// <param name="tLoc"></param>
+        /// <param name="rPlyrWt"></param>
+        public Football(string tName, string sName, string cName, string rTurf, string tLoc, int rPlyrWt)
+            : base (tName, sName, cName)
+        {
+            reqTurfType = rTurf;
+            teamLocation = tLoc;
+            reqPlayerWeight = rPlyrWt;
+        }
+        #endregion Constructors
+
+        #region Methods
+        public override string ToString()
+        {
+            return base.ToString() + " " + reqTurfType;
+        }
+
+        public override int reqPlayers()
+        {
+            return 11;
+        }
+        #endregion Methods
+    }
+}
---
+++ Tennis.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamsLib;
+
+namespace SportSubClass
+{
+    class Tennis : Team
+    {
+        #region Private Class Fields
+        private string reqEquipment;
+        private string playingField;
+        private int pointsToWin;
+        #endregion Private Class Fields
+
+        #region Public Properties
+        public string ReqEquipment { get => reqEquipment; set => reqEquipment = value; }
+        public string PlayingField { get => playingField; set => playingField = value; }
+        public int PointsToWin { get => pointsToWin; set => pointsToWin = value; }
+        #endregion Public Properties
+
+        #region Constructors
+        /// <summary>
+        /// Default No-Arg
+        /// </summary>
+        public Tennis()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Builds Complete Tennis Team
+        /// </summary>
+        /// <param name="tName"></param>
+        /// <param name="sName"></param>
+        /// <param name="cName"></param>
+        /// <param name="reqEquip"></param>
+        /// <param name="plyField"></param>
+        /// <param name="ptsToWin"></param>
+        public Tennis(string tName, string sName, string cName, string reqEquip,
+            string plyField, int ptsToWin)
+            : base(tName, sName, cName)
+        {
+            reqEquipment = reqEquip;
+            playingField = plyField;
+            pointsToWin = ptsToWin;
+        }
+        #endregion Constructors
+
+        #region Methods
+        public override string ToString()
+        {
+            return base.ToString() + " " + reqEquipment;
+        }
+
+        public override int reqPlayers()
+        {
+            return 2;
+        }
+        #endregion Methods
+    }
+
+}
